f25d4712de2ccc137567a1191a8185b4cc77e136
mher321 <mher321@gmail.com>
2019-02-08 23:42:56 +0400

    Initial commit

---
+++ src/auditorium/ObjectsTest.java
@@ -0,0 +1,18 @@
+package auditorium;
+
+public class ObjectsTest {
+    public void main(String[] args) {
+        Home home1 = new Home();
+    }
+
+    public class Home{
+
+        public Home(){
+
+        }
+
+        int y;
+        String name;
+
+    }
+}
