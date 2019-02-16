923475cfd9f1cdd006abd6d96fdaf60b9d9d729e
mher321 <mher321@gmail.com>
2019-02-17 00:18:05 +0400

    Initial commit

--- src/auditorium/classintro/RectangleTest.java
+++ src/auditorium/classintro/RectangleTest.java
@@ -3,17 +3,17 @@ package auditorium.classintro;
 public class RectangleTest {
 
     public static void main(String[] args) {
-//        int width1 = 2;
-//        int width2 = 2;
-//        int height1 = 2;
-//        int height2 = 2;
+        int width1 = 2;
+        int width2 = 2;
+        int height1 = 2;
+        int height2 = 2;
 
-//
-//        int s1 = width1 * height1;
-//        int s2 = width2 * height2;
-//
-//        int p1 = getPerimeter(width1, height1);
-//        int p2 = getPerimeter(width2, height2);
+
+        int s1 = width1 * height1;
+        int s2 = width2 * height2;
+
+        int p1 = getPerimeter(width1, height1);
+        int p2 = getPerimeter(width2, height2);
 
         Rectangle rect1 = new Rectangle(4, 5);
         int perimeter = rect1.getPerimeter();
