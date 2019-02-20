65c12eac9fccfece805e30240c564c0b41c23951
mher321 <mher321@gmail.com>
2019-02-20 11:51:08 +0400

    Initial commit

---
+++ src/homework/Slove.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Slove {
+    public static void main(String[] args) {
+        double x,y;
+        x =14;
+
+        y = x/209+x*(29-(13*13)+x/3);
+
+        System.out.println(y);
+    }
+}
