195bdb98c6002fdc1c9a06c1edb4e61497f6b438
mher321 <mher321@gmail.com>
2019-03-31 11:48:00 +0400

    Initial commit

--- src/auditorium/bracechecker/BraceChecker.java
+++ src/auditorium/bracechecker/BraceChecker.java
@@ -2,19 +2,90 @@ package auditorium.bracechecker;
 
 public class BraceChecker {
     public Stack stack;
-    public String resultMassage;
+    public String resultMassage = "There is No error";
 
-    //TODO add constructor
+    BraceChecker() {
+        stack = new Stack();
+    }
 
     public void parse(String txt) {
 
-        for (int i = 0; i < txt.length(); i++) {
-            char ch = txt.charAt(i);
+        char ch = 0;
+        char stkLastElement = 0;
+        int i = 0;
+        lab:
+        for (; i < txt.length(); i++) {
+            ch = txt.charAt(i);
+            switch (ch) {
+                case '[':
+                case '{':
+                case '(':
+                    stack.push(ch);
+                    break;
+                case '}':
+                    stkLastElement = stack.pop();
+                    if (stkLastElement != '{') {
+                        break lab;
+                    }
+                    break;
+                case ']':
+                    stkLastElement = stack.pop();
+                    if (stkLastElement != '[') {
+                        break lab;
+                    }
+                    break;
 
-            if (ch == '(' || ch == '{' || ch == '[') {
-                System.out.println("pars is open");
-
+                case ')':
+                    stkLastElement = stack.pop();
+                    if (stkLastElement != '(') {
+                        break lab;
+                    }
             }
+        }
+        if (i < txt.length()) {
+            if (stkLastElement == 0) {
+                resultMassage = "Error: Closed '" + ch + "' but not opened ";
+            } else {
+                resultMassage = "Error: Opened '" + stkLastElement + "' but closed '" + ch + "'.";
+            }
+        } else if ((stkLastElement = stack.pop()) != 0) {
+            resultMassage = "Error: Opened '" + stkLastElement + "' but not closed '";
         }
     }
 }
+
+class BrTest {
+    public static void main(String[] args) {
+        BraceChecker braceChecker = new BraceChecker();
+        braceChecker.parse("I (can open and close} ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I [can open  and close} ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I {can open  and close} ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I [can open  and close) ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I [can open  and close] ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I (can open  and close] ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I {can open  and close) ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I {can open  and close] ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I {can open  and close ");
+        System.out.println(braceChecker.resultMassage);
+
+        braceChecker.parse("I can open  and close} ");
+        System.out.println(braceChecker.resultMassage);
+    }
+}
+
--- src/auditorium/bracechecker/Stack.java
+++ src/auditorium/bracechecker/Stack.java
@@ -1,26 +1,34 @@
 package auditorium.bracechecker;
 
 public class Stack {
-    char[] array;
-    int tos = -1;
+    public static final int DEFAULT_SIZE = 16;
 
-    Stack() {
-        array = new char[6];
+    private char[] array;
+    private int tos = -1;
+
+    public Stack() {
+        this(DEFAULT_SIZE);
+    }
+
+    public Stack(int size) {
+        array = new char[size];
     }
 
     void push(char ch) {
         if (tos == array.length - 1) {
-            System.out.println("full");
-
+            System.out.println("stack is full");
+        } else {
             array[++tos] = ch;
         }
     }
 
     char pop() {
-        if (tos == -1) {
-            System.out.println("no parser");
+        if (tos < 0) {
+            System.out.println("stack is empty ");
+            return 0;
         }
-        return 0;
+        return array[tos--];
     }
+
 }
 
